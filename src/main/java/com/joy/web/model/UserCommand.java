3e5db0ff64fcfc6bd003c0a2602874c0d1ec757c
Jin ukCha <burningsweetpotato@gmail.com>
2016-10-26 18:38:13 +0900

    test model 생성

---
+++ src/main/java/com/joy/web/model/UserCommand.java
@@ -0,0 +1,12 @@
+package com.joy.web.model;
+
+public class UserCommand {
+	String userName;
+	String passWord;
+	String userEmail;
+	String profileImage;
+	String studentNO;
+	String phoneNO;
+	Integer admin;
+	
+}
